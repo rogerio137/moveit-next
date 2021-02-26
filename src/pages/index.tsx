803e10368675aece53b45b4f295c56b594e9bfe0
Rogerio <barrucalleortiz@gmail.com>
2021-02-26 12:00:10 -0300

    Adicionando tela modal de level

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,30 +5,55 @@ import { Profile } from '../components/Profile';
 
 import styles from '../styles/pages/Home.module.css';
 import Head from 'next/head';
+import { GetServerSideProps } from 'next';
 import { ChallengeBox } from '../components/ChallengeBox';
 import { CountdownProvider } from '../contexts/CountdownContext';
+import { ChallengesProvider } from '../contexts/ChallengesContext';
 
 
-export default function Home() {
+interface HomeProps {
+  level: number;
+  challengesCompleted: number;
+  currentExperience: number;
+}
+
+export default function Home(props:HomeProps) {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Inicio | move-it</title>
-      </Head>
-      <ExperienceBar />
+    <ChallengesProvider level={props.level} 
+                        currentExperience={props.currentExperience}
+                        challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Inicio | move-it</title>
+        </Head>
+        <ExperienceBar />
 
-      <CountdownProvider>
-        <section>
-          <div >
-            <Profile />
-            <CompleteChallenges />
-            <Countdown />
-          </div>
-          <div>
-            <ChallengeBox />
-          </div>
-        </section>
-      </CountdownProvider>
-    </div>
+        <CountdownProvider>
+          <section>
+            <div >
+              <Profile />
+              <CompleteChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+
+  const { level, challengesCompleted, currentExperience } = ctx.req.cookies;
+  return {
+    props: {
+      level: Number(level),
+      challengesCompleted: Number(challengesCompleted),
+      currentExperience: Number(currentExperience)
+    }
+  }
 }
